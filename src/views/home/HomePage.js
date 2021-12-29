e41b9e77d000837bf8db0fb5993c3a7a8bb9a5b7
Wilfred <wilfredbayudan@gmail.com>
2021-12-28 18:33:52 -1000

    marker color update

--- src/views/home/HomePage.js
+++ src/views/home/HomePage.js
@@ -4,13 +4,26 @@ const HomePage = ({ appState }) => {
   
   const { setMarkers, destinations } = appState;
 
-  const generateMarkerColor = () => {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+  // const generateMarkerColor = () => {
+  //   let letters = '0123456789ABCDEF';
+  //   let color = '#';
+  //   for (let i = 0; i < 6; i++) {
+  //     color += letters[Math.floor(Math.random() * 16)];
+  //   }
+  //   return color;
+  // }
+
+  const stringToColour = (str) => {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
     }
-    return color;
+    let colour = '#';
+    for (let i = 0; i < 3; i++) {
+      let value = (hash >> (i * 8)) & 0xFF;
+      colour += ('00' + value.toString(16)).substr(-2);
+    }
+    return colour;
   }
 
   useEffect(() => {
@@ -18,7 +31,7 @@ const HomePage = ({ appState }) => {
       setMarkers(destinations.filter(filterDestination => filterDestination.bucketlists.length > 0).map(destination => {
         return {
           ...destination,
-          color: generateMarkerColor()
+          color: stringToColour(destination.label)
         }
       }));
     }
